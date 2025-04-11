33222bad800808ac93d5ea2107a57afefc58e028
Lukas Wolf <lukas.wolf00@gmx.at>
2025-04-11 11:17:20 +0200

    lkjk

--- main/buttons/button.c
+++ main/buttons/button.c
@@ -14,7 +14,7 @@ void randButtonInit() {
 void cheatButtonInit() {
     gpio_config_t io_conf = {.pin_bit_mask = (1ULL << CHEAT_BUTTON_GPIO),
                              .mode = GPIO_MODE_INPUT,
-                             .pull_up_en = GPIO_PULLUP_ENABLE,
+                             .pull_up_en = GPIO_PULLDOWN_ENABLE,
                              .intr_type = GPIO_INTR_DISABLE};
     gpio_config(&io_conf);
   }
