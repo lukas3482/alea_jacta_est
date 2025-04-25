7e0f17cf7941b1b2ee3b1e4589d9a9e20c1b6340
Lukas Wolf <lukas.wolf00@gmx.at>
2025-04-25 09:16:49 +0200

    added matrix.c & h

--- main/matrix/matrix.c
+++ main/matrix/matrix.c
@@ -1,6 +1,13 @@
 #include "matrix.h"
+#include "led_strip.h"
+#include "esp_log.h"
+#include "freertos/task.h"
+#include "rand/rand.h"
 
-static int matrix [6][25] = {    
+#define LED_GPIO 8
+static led_strip_handle_t led_strip;
+
+static uint8_t matrix [6][25] = {    
     {
         0,0,0,0,0,
         1,0,0,1,0,
@@ -44,5 +51,51 @@ static int matrix [6][25] = {
         0,1,0,0,0
     },
 };
+
+static void rauschen(void){
+    for(int i = 0; i < 20; i++){
+        led_strip_clear(led_strip);
+
+        led_strip_set_pixel(led_strip, nextRand() % 25, 10, 10, 10);
+        led_strip_set_pixel(led_strip, nextRand() % 25, 10, 10, 10);
+        led_strip_set_pixel(led_strip, nextRand() % 25, 10, 10, 10);
+        led_strip_set_pixel(led_strip, nextRand() % 25, 10, 10, 10);
+        led_strip_set_pixel(led_strip, nextRand() % 25, 10, 10, 10);
+
+        led_strip_refresh(led_strip);
+
+        vTaskDelay(50 / 10);
+    }
+    led_strip_clear(led_strip);
+    led_strip_refresh(led_strip);
+}
+
+void displayMatrix(uint8_t rand){
+    rauschen();
+    led_strip_clear(led_strip);
+
+    for(int i = 0; i < 25; i++){
+        led_strip_set_pixel(led_strip, i, matrix[rand][i] * 10, matrix[rand][i] * 10, matrix[rand][i] * 10);
+    }
+
+    led_strip_refresh(led_strip);
+    
+}
+
+void configure_led(void){
+    //ESP_LOGI(TAG, "Example configured to blink addressable LED!");
+    led_strip_config_t strip_config = {
+        .strip_gpio_num = LED_GPIO,
+        .max_leds = 25,
+    };
+    led_strip_rmt_config_t rmt_config = {
+        .resolution_hz = 10 * 1000 * 1000,
+        .flags.with_dma = false,
+    };
+    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
+
+    led_strip_clear(led_strip);
+    led_strip_refresh(led_strip);
+}
 
 
--- main/matrix/matrix.h
+++ main/matrix/matrix.h
@@ -1,9 +1,10 @@
 #ifndef RGB_matrix
 #define RGB_matrix
 
+#include <inttypes.h>
 
-
-
+void displayMatrix(uint8_t rand);
+void configure_led(void);
 
 
 #endif
